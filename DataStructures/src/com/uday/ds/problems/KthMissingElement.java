b18526427fac64de22803acb8cfd8ef73fd67ae8
udayv1985 <43344239+udayv1985@users.noreply.github.com>
2020-10-19 17:24:09 +0530

    Update KthMissingElement.java

--- DataStructures/src/com/uday/ds/problems/KthMissingElement.java
+++ DataStructures/src/com/uday/ds/problems/KthMissingElement.java
@@ -1,5 +1,24 @@
 package com.uday.ds.problems;
 
+/*
+Given an increasing sequence a[], we need to find the K-th missing contiguous element in the increasing sequence which is not present in the sequence. If no k-th missing element is there output -1. 
+Examples : 
+
+Input : a[] = {2, 3, 5, 9, 10};   
+        k = 1;
+Output : 4
+Explanation: Missing Element in the increasing 
+sequence are {4, 6, 7, 8}. So k-th missing element
+is 4
+
+Input : a[] = {2, 3, 5, 9, 10, 11, 12};       
+        k = 4;
+Output : 8
+Explanation: missing element in the increasing 
+sequence are {4, 6, 7, 8}  so k-th missing 
+element is 8
+*/
+
 public class KthMissingElement {
 
     public static int find(int[] a, int k){
